383a79baf4340b992ba6f7ac9c9055a4d78cc956
Inoyu <117442042+Xia7an@users.noreply.github.com>
2025-08-24 14:26:06 +0900

    add: gltfast

--- Assets/Scripts/Food3DModel/Interface/IFood3DModelViewModel.cs
+++ Assets/Scripts/Food3DModel/Interface/IFood3DModelViewModel.cs
@@ -1,7 +1,10 @@
+using R3;
+using UnityEngine;
+
 namespace Food3DModel.Interface
 {
     public interface IFood3DModelViewModel
     {
-        
+        ReactiveProperty<GameObject> Food3DModel { get; }
     }
 }
--- Assets/Scripts/Food3DModel/ViewModel/Food3DModelViewModel.cs
+++ Assets/Scripts/Food3DModel/ViewModel/Food3DModelViewModel.cs
@@ -1,7 +1,43 @@
+using Food3DModel.Interface;
+using R3;
+using UnityEngine;
+using Zenject;
+
 namespace Food3DModel.ViewModel
 {
-    public class Food3DModelViewModel
+    public class Food3DModelViewModel: IFood3DModelViewModel
     {
+        [Inject] IFood3DModelAPIHandler _food3DModelAPIHandler;
         
+        public ReactiveProperty<GameObject> Food3DModel { get; }
+        
+        public Food3DModelViewModel()
+        {
+            Food3DModel = new ReactiveProperty<GameObject>(null);
+        }
+        
+        public void LoadFood3DModel(string foodName)
+        {
+            var apiBody = _food3DModelAPIHandler.Request(foodName);
+            if (apiBody.GlbData != null)
+            {
+                var tempFilePath = System.IO.Path.Combine(Application.temporaryCachePath, "tempFood.glb");
+                System.IO.File.WriteAllBytes(tempFilePath, apiBody.GlbData);
+                
+                var gltfObject = GLTFast.GltfImport.LoadFromFile(tempFilePath).Result;
+                if (gltfObject != null)
+                {
+                    Food3DModel.Value = gltfObject;
+                }
+                else
+                {
+                    Debug.LogError("Failed to load GLTF object from file.");
+                }
+            }
+            else
+            {
+                Debug.LogError("Received null GLB data from API.");
+            }
+        }
     }
 }
