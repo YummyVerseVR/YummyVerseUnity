7279bc11074f56e9780673f7757fcc73202209ef
Inoyu <117442042+Xia7an@users.noreply.github.com>
2025-09-08 01:41:17 +0900

    ADBで！！！デバッグ機能を！！！有効にしろ！！！じゃないと！！！QRを！！！認識しない！！！

--- Assets/FoodDB/Scripts/God/QRTrackTest.cs
+++ Assets/FoodDB/Scripts/God/QRTrackTest.cs
@@ -9,10 +9,87 @@ public class QRTrackTest : MonoBehaviour
         Debug.Log("すた〜と！");
     }
     
+   
+
+
+    //                                    `       `       `       `
+    //   `  `    `    `    `    `    `        `       `       `        `    `    `    `    `    `    `
+    //                                  `
+    //                                           `       `       `  `     `
+    // `  `   `   `  `  `   `  `  `        `  `     `       `                 `   `  `  `   `  `  `
+    //                               `                 `       `       `                             `
+    //                                  `             ...
+    //     `  `  `       `        `             `  .MMMMMN,       `       `  `  `        `        `
+    //  `           `       `  `           `      .MMMMMMMMx  `      `             `  `     `  `
+    //                 `             `  `     `  .MMMMMMMMMMp           `                            `
+    //                                          .MMMMMMMMMMMMb   `          `   `        `
+    //    `  `  `  `             `             -MMMMMF  TMMMMMb     `               `        `   `
+    //                  `  `  `     `     `   JMMMMMF    ?MMMMMh       `                            `
+    //  `                              `     dMMMMM$      (MMMMMN.        `           `   `
+    //              `                      .MMMMMM3        ,MMMMMN,          `  `  `          `       `
+    //     `  `  `     `        `  `      .MMMMMM^          ,MMMMMN,`  `                         `
+    //                    `  `        `  .MMMMMM'   .+NNg,   .MMMMMM,                  `   `        `
+    //  `                               .MMMMM#`   JMMMMMMb    HMMMMM,    `   `
+    //             `  `                .MMMMM@     MMMMMMMM     WMMMMMp          `  `         `
+    //     `  `          `     `  `   .MMMMM@      MMMMMMM#      UMMMMMp     `          `  `     `   `
+    //  `        `          `        .MMMMMF       dMMMMMM#       TMMMMMb
+    //                              JMMMMMF        JMMMMMMF        ?MMMMMh           `
+    //              `  `           dMMMMMD         (MMMMMMF         JMMMMMN.   `  `          `  `  `  `
+    //    `  `  `         `   `  .MMMMMM3      `   ,MMMMMM%          ,MMMMMN,           `
+    //                          .MMMMMM^           .MMMMMM}   `  `    ,MMMMMN,             `
+    //  `           `          .MMMMMM'             MMMMMM`            .MMMMMM,     `
+    //           `     `  `  `.MMMMM#`       `  `   MMMMMM               HMMMMM,       `      `  `  `
+    //     `  `              .MMMMM#                MMMMM#                WMMMMMp         `
+    //                      .MMMMM@                 dMMMMF   `  `  `       UMMMMMp  `                 `
+    //  `          `       .MMMMMF         `     `  (MMMMF                  TMMMMMb          `
+    //          `     `   JMMMMMF             `       7"`             `      ?MMMMMh    `       `  `
+    //     `             dMMMMMD        `                    `                JMMMMMN.
+    //        `        .MMMMMM3                     .MMMN,       `             ,MMMMMN,
+    //  `          `  .MMMMMM^      `      `   `   .MMMMMM]         `           ,MMMMMN,
+    //               .MMMMMM'          `           ,MMMMMM\                      .MMMMMM,
+    //     `   `    .MMMMMM`                         TW""`    `                    MMMMMM,
+    //            `.MMMMMMNJ.......................................................MMMMMMM|
+    //  `          (MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMF
+    //      `       TMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM@
+    //         `      ?7777777777777777777777777777777777777777777777777777777777777777!
+    //  `
+    //     `
+    //        `  `
+    //
+    //  `           `
+    //     `             `    `    `    `    `    `  `  `  `  `
+    //        `  `                                               `
+    //               `      `                                       `
+    //  `               `       `   `  `  `   `  `            ` 
+    // ADBコマンドでExperimental FeaturesをONにしておくこと!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    // ADBコマンドでExperimental FeaturesをONにしておくこと!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    // ADBコマンドでExperimental FeaturesをONにしておくこと!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    // ADBコマンドでExperimental FeaturesをONにしておくこと!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    // ADBコマンドでExperimental FeaturesをONにしておくこと!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    // ADBコマンドでExperimental FeaturesをONにしておくこと!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    // ADBコマンドでExperimental FeaturesをONにしておくこと!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    // ADBコマンドでExperimental FeaturesをONにしておくこと!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    // ADBコマンドでExperimental FeaturesをONにしておくこと!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    // ADBコマンドでExperimental FeaturesをONにしておくこと!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    // ADBコマンドでExperimental FeaturesをONにしておくこと!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    // ADBコマンドでExperimental FeaturesをONにしておくこと!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    // ADBコマンドでExperimental FeaturesをONにしておくこと!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    // ADBコマンドでExperimental FeaturesをONにしておくこと!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    // ADBコマンドでExperimental FeaturesをONにしておくこと!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    // ADBコマンドでExperimental FeaturesをONにしておくこと!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    // ADBコマンドでExperimental FeaturesをONにしておくこと!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    // ADBコマンドでExperimental FeaturesをONにしておくこと!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    // ADBコマンドでExperimental FeaturesをONにしておくこと!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    // ADBコマンドでExperimental FeaturesをONにしておくこと!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    // ADBコマンドでExperimental FeaturesをONにしておくこと!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    // ADBコマンドでExperimental FeaturesをONにしておくこと!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    
     public void OnTrackableAdded(MRUKTrackable trackable)
     {
-        Debug.Log($"Trackable of type {trackable.TrackableType} added.");
-        Instantiate(testFood, trackable.transform);
+        if (trackable.TrackableType == OVRAnchor.TrackableType.QRCode)
+        {
+            Instantiate(testFood, trackable.transform);
+        }
     }
 
     public void OnTrackableRemoved(MRUKTrackable trackable)
