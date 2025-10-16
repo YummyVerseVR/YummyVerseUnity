e46d286a3892b7fb5d1b224cca714ed4badf6458
Inoyu <inoyu0329@gmail.com>
2025-10-16 23:49:06 +0900

    wip: webカメラつながらない

--- Assets/PCVR/Scripts/FoodInstantiater.cs
+++ Assets/PCVR/Scripts/FoodInstantiater.cs
@@ -1,15 +1,23 @@
+using System;
 using PCVR.Model.Interfaces;
 using UnityEngine;
 using Zenject;
+using GLTFast;
 
 namespace PCVR
 {
     public class FoodInstantiater : MonoBehaviour
     {
         [Inject] private IQRCodeManager QRCodeManager;
+        private string endpoint = "http://localhost:8001/";
+
         void Start()
         {
-        
+            Uri uri = new Uri(endpoint + QRCodeManager.UserId + "/model");
+            var gltfImport = new GltfImport();
+            gltfImport.Load(uri);
+            gltfImport.InstantiateMainSceneAsync(transform);
         }
+
     }
 }
