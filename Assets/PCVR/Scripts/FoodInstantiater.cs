6ba482d8fcd97c40de3bcc2d22b83d86b5ab4b44
Inoyu <inoyu0329@gmail.com>
2025-10-22 23:56:55 +0900

    wip: エンドポイントを最新に変更

--- Assets/PCVR/Scripts/FoodInstantiater.cs
+++ Assets/PCVR/Scripts/FoodInstantiater.cs
@@ -4,6 +4,7 @@ using PCVR.Model.Interfaces;
 using UnityEngine;
 using Zenject;
 using GLTFast;
+using Oculus.Interaction;
 using UnityEngine.SceneManagement;
 
 namespace PCVR
@@ -11,7 +12,9 @@ namespace PCVR
     public class FoodInstantiater : MonoBehaviour
     {
         [Inject] private IQRCodeManager QRCodeManager;
-        private string endpoint = "http://localhost:8001/";
+        private string endpoint = "http://yummy-control-server.upiscium.f5.si:8000/";
+        // private string endpoint = "http://localhost:8001/";
+        private string id = "7b998836-903e-4878-ae8e-839a2ef13373";
 
         void Start()
         {
@@ -20,6 +23,7 @@ namespace PCVR
 
         private async UniTask InstantiateGLB()
         {
+            // string url = endpoint + QRCodeManager.UserId + "/model/";
             string url = endpoint + QRCodeManager.UserId + "/model/";
             Debug.Log(url);
             var gltfImport = new GltfImport();
@@ -28,7 +32,8 @@ namespace PCVR
             if (success)
             {
                 // 読み込んだglTFのメインシーンを、このスクリプトがアタッチされているGameObjectの子として非同期でインスタンス化します。
-                await gltfImport.InstantiateMainSceneAsync(transform);
+                await gltfImport.InstantiateMainSceneAsync(this.transform);
+                this.transform.localScale = Vector3.one * 0.3f;
                 Debug.Log("Successfully loaded and instantiated GLB from URL.");
             }
             else
---
+++ Assets/PCVR/Scripts/View/ControllerController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Cysharp.Threading.Tasks;
+using R3;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace PCVR.Scripts.View
+{
+    public class ControllerController : MonoBehaviour
+    {
+
+        private string endpoint = "http://localhost:8001/ping";
+        private ReactiveProperty<bool> isEating = new ReactiveProperty<bool>(false);
+
+        private void Start()
+        {
+            NotifyEating().Forget();
+            isEating.Where(v => v).Subscribe(v =>
+            {
+                NotifyEating().Forget();
+            });
+        }
+
+        private void Update()
+        {
+            if ((Camera.main.transform.position - this.transform.position).magnitude < 0.15f)
+            {
+                isEating.Value = true;
+            }
+        }
+
+        private async UniTask NotifyEating()
+        {
+            UnityWebRequest request = UnityWebRequest.Get(endpoint);
+            await request.SendWebRequest();
+            Debug.Log(request.downloadHandler.text);
+        }
+    }
+
+}
