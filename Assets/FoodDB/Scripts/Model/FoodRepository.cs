1c2f7ed78bd057c049b1de525ccf465e38a68803
so-so-F2 <isomotosoutaro@gmail.com>
2025-10-23 18:33:24 +0900

    fix: 治った～～～～

--- Assets/FoodDB/Scripts/Model/FoodRepository.cs
+++ Assets/FoodDB/Scripts/Model/FoodRepository.cs
@@ -66,7 +66,6 @@ namespace Food3DModel.Model
                 }
                 else
                 {
-                    FoodTransform.Value.Find("world").Rotate(90, 0,0);
                     Debug.Log("[ModelLoader] Successfully loaded and instantiated model.");
                     // Food3DModel.Value.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f); // スケール調整
                 }
